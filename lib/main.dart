c9fb5fcef63469e3f848b47937aff569a322d0ce
Athar Adista <athar0165@gmail.com>
2023-12-12 17:04:41 +0700

    udah bisa pinjam buku

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
           useMaterial3: true,
         ),
-        home: ProductPage(),
+        home: MyHomePage(),
       )
     );
   }
