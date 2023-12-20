b3879fb4d37ed4267bf95f5f9b580f92551c43a3
Athar Adista <athar0165@gmail.com>
2023-12-20 23:10:34 +0700

    fix style navbar

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -46,7 +46,12 @@ class _BottomNavState extends State<BottomNav> {
     return Scaffold(
       body: _pages[_selectedIndex],
       bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: 2.0, color: Colors.brown), // Atur warna dan lebar sesuai keinginan
+        ),
         color: Color.fromARGB(255, 240, 229, 210),
+        ),
         child: Padding(
           padding: const EdgeInsets.fromLTRB(20, 2, 20, 2),
           child: GNav(
